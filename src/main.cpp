eae84ea935c47fb93c9d1103fc2db84394fb0aed
Barry Robinson <barry.w.robinson64@gmail.com>
2024-08-24 17:51:50 +0100

    put main together

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,57 @@
 
 #include <librotate.h>
 
+static inline void do_help() {
+	std::cout << "./build/rotate -i <infile.bin> -o <outfile.bin> -r [left | right]" << std::endl;
+    std::cout << "./build/rotate -s <HEX STRING>" << std::endl;
+}
+
 int main(int argc, char **argv)
 {
-    Buffer b("test.bin","test-out.bin");
+    Args args(argc,argv);
 
-    b.rotate_left();
+    args.add_string_value("-r","--rotate","left");
+    args.add_string_value("-i","--in-file","");
+    args.add_string_value("-o","--out-file","");
+    args.add_string_value("-s","--string","");
+
+
+    std::string rdir = args.get_string_value("-r");
+    std::string str  = args.get_string_value("-s");
+    std::string ifl  = args.get_string_value("-i");
+    std::string ofl  = args.get_string_value("-o");
+
+    if(str != "") {
+        // Do rotation on the hex string
+        HexString b(str);
+        b.init();
+
+        if(rdir == "left") {
+            b.rotate_left();
+        }
+        else {
+            b.rotate_right();
+        }
+        b.reset();
+
+        std::string out = b.render();
+
+        std::cout << "Rotated string " << rdir << ": " << out << std::endl;
+
+        exit(0);
+    }
+    else if(ifl != "" && ofl != "") {
+        Buffer b(ifl,ofl);
+        b.init();
+
+        if(rdir == "left") {
+            b.rotate_left();
+        }
+        else {
+            b.rotate_right();
+        }
+
+    }
+
     return 1;
 }
