ebf2c0f6a4bf4baae12b6c953767ba3bd7524bee
Barry Robinson <barry.w.robinson64@gmail.com>
2024-08-26 10:03:10 +0100

    add valgrind to dependancies

--- depenancies.sh
+++ depenancies.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo apt install build-essential make sudo git cmake doxygen cppcheck -y 
+sudo apt install build-essential make sudo git cmake doxygen cppcheck valgrind -y 
 
 git clone https://github.com/google/googletest.git
 cd googletest && mkdir -p build && cd build && cmake .. && cmake --build . && \sudo cmake --install .
